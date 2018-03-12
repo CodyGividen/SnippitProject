23806692d11b8e4e36c4c8c99d6da7f2d05f72c0
CodyGividen <codygividen5@gmail.com>
2018-03-12 16:10:27 -0400

    did snippetProject. created methods that take variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+        String petName = "Jeff";
+        double num1 = 9.323;
+        double num2 = 5.555;
+
+        private String takePetName(String petName) {
+            return petName;
+        }
+        private double multiplication(double num1, double num2) {
+            System.out.println(num1 + num2);
+            return num1 * num2;
+
+        }
+}
